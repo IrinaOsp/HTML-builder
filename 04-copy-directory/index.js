a308cee3cf7d5aa4f2768d305dd70794cc23bf88
“Irina <irina.osipova2012@gmail.com>
2023-05-05 17:44:59 +0200

    feat: task 04-copy-directory solved

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+const { COPYFILE_EXCL } = fs.constants;
 
+const pathCopyFolder = path.join(__dirname, 'files-copy');
+const pathFolder = path.join(__dirname, 'files');
+
+fs.mkdir(pathCopyFolder, { recursive: true, force: true, }, (err) => {
+  if (err) return;
+
+  fs.readdir(pathFolder, {withFileTypes: true}, (err, files) => {
+    if (err) return;
+
+    files.forEach(file => {
+      if (file.isFile()) {
+        fs.copyFile(path.resolve(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), COPYFILE_EXCL, (err)=> {
+          if (err) {
+            console.log(`File ${file.name} already exists`);
+          } else {
+            console.log(`File ${file.name} copied`);
+          }
+        });
+      }
+    });
+  });
+});
