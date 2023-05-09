a47e02208e974d351a45a0ca1751236376446888
“Irina <irina.osipova2012@gmail.com>
2023-05-09 11:43:43 +0200

    fix: fix folder clearence in 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,20 +7,30 @@ const pathFolder = path.join(__dirname, 'files');
 
 fs.mkdir(pathCopyFolder, { recursive: true, force: true, }, (err) => {
   if (err) return;
+});
 
-  fs.readdir(pathFolder, {withFileTypes: true}, (err, files) => {
-    if (err) return;
-
-    files.forEach(file => {
-      if (file.isFile()) {
-        fs.copyFile(path.resolve(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), COPYFILE_EXCL, (err)=> {
-          if (err) {
-            console.log(`File ${file.name} already exists`);
-          } else {
-            console.log(`File ${file.name} copied`);
-          }
-        });
-      }
+fs.readdir(pathCopyFolder, (err, files) => {
+  if (err) return;
+  files.forEach(file => {
+    fs.unlink(path.join(pathCopyFolder, file), (err) => {
+      if (err) throw err;
     });
   });
 });
+
+fs.readdir(pathFolder, {withFileTypes: true}, (err, files) => {
+  if (err) return;
+
+  files.forEach(file => {
+    if (file.isFile()) {
+      fs.copyFile(path.resolve(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), COPYFILE_EXCL, (err)=> {
+        if (err) {
+          console.log(`File ${file.name} already exists`);
+        } else {
+          console.log(`File ${file.name} copied`);
+        }
+      });
+    }
+  });
+});
+
